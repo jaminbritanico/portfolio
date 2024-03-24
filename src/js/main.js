9622560ae7e158027c9b9f1da62837d4ef8ac9b1
Jamin Britanico <32903539+jaminbritanico@users.noreply.github.com>
2024-03-25 00:23:20 +0800

    Changes: - testing media queries for responsive

--- src/js/main.js
+++ src/js/main.js
@@ -1,15 +1,21 @@
+$(window).on('resize', function() {
+	resize()
+});
+
+function resize() {
+	
+}
+
 $("#btnDarkMode").click(function(){
 	if($("body").hasClass("bodyDarkMode"))
 	{
 		$("body").removeClass("bodyDarkMode");
-		$("#btnDarkMode > i").addClass("bxs-moon");
-		$("#btnDarkMode > i").removeClass("bxs-sun");
+		$("#btnDarkMode").removeClass("bxs-sun").addClass("bxs-moon");
 	}
 	else
 	{
 		$("body").addClass("bodyDarkMode");
-		$("#btnDarkMode > i").removeClass("bxs-moon");
-		$("#btnDarkMode > i").addClass("bxs-sun");
+		$("#btnDarkMode").removeClass("bxs-moon").addClass("bxs-sun");
 	}
 });
   
