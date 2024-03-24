f6d1bf22c4449eddf85e7fc1dea1f040a297e66e
Jamin Britanico <32903539+jaminbritanico@users.noreply.github.com>
2024-03-24 22:23:37 +0800

    Changes - import boxicons - darkmode button change to icon and align to right

--- src/js/main.js
+++ src/js/main.js
@@ -1,8 +1,15 @@
-
 $("#btnDarkMode").click(function(){
 	if($("body").hasClass("bodyDarkMode"))
+	{
 		$("body").removeClass("bodyDarkMode");
+		$("#btnDarkMode > i").addClass("bxs-moon");
+		$("#btnDarkMode > i").removeClass("bxs-sun");
+	}
 	else
+	{
 		$("body").addClass("bodyDarkMode");
+		$("#btnDarkMode > i").removeClass("bxs-moon");
+		$("#btnDarkMode > i").addClass("bxs-sun");
+	}
 });
   
