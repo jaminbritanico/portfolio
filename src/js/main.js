cc3937224c0ce17b586702ddba4ce55ab0f5a940
Jamin Britanico <32903539+jaminbritanico@users.noreply.github.com>
2024-03-26 00:22:51 +0800

    Changes - add particle js - add function to home - add hover one design

--- src/js/main.js
+++ src/js/main.js
@@ -1,8 +1,14 @@
 $("#btnDarkMode").click(function(){
 	$("body").toggleClass("darkMode");
 	if($("body").hasClass("darkMode"))
+	{
+		setParticles("#FFFAFA");
 		$("#btnDarkMode").removeClass("bxs-moon").addClass("bxs-sun");
+	}
 	else
+	{
+		setParticles("#1E1E1E");
 		$("#btnDarkMode").removeClass("bxs-sun").addClass("bxs-moon");
+	}
 });
   
